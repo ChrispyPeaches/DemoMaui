986a8c2f72dbf93cc00a6f451fab482554fa3b4b
ChrispyPeaches <gameboy9478@gmail.com>
2023-12-11 13:28:59 -0600

    Add project files.

---
+++ src/DemoMauiApp/External/WebApplication1/IWebApplicationApi.cs
@@ -0,0 +1,11 @@
+﻿using DemoLibrary;
+using Refit;
+
+namespace DemoMauiApp.External.DemoAPI
+{
+    public interface IWebApplicationApi
+    {
+        [Get("/WeatherForecast")]
+        Task<IEnumerable<WeatherForecast>> GetWeatherForecasts();
+    }
+}
---
+++ src/DemoMauiApp/MauiProgram.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Logging;
+using Refit;
+
+namespace DemoMauiApp
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                });
+
+            builder.Services.AddMauiBlazorWebView();
+            builder.Services
+                .AddRefitClient<External.DemoAPI.IWebApplicationApi>()
+                .ConfigureHttpClient(c => c.BaseAddress = new Uri("https://localhost:7292"));
+
+#if DEBUG
+    		builder.Services.AddBlazorWebViewDeveloperTools();
+    		builder.Logging.AddDebug();
+#endif
+
+            return builder.Build();
+        }
+    }
+}
